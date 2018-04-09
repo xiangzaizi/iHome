41819173672c164f0521198f35edebf1c60f6f5c
xiangzaizi <skyxiang.jiang@hotmail.com>
2018-04-09 19:59:55 +0800

    加载favicon.ico图标,部分逻辑代码测试优化

--- iHome/utils/common.py
+++ iHome/utils/common.py
@@ -6,6 +6,6 @@ class RegexConverter(BaseConverter):
     """自定义路由转换器, 用户匹配静态文件路由"""
     def __init__(self, url_map, *args):
         super(RegexConverter, self).__init__(url_map)
-
+        # 定义的规则
         self.regex = args[0]
         # 注意自定义的路由转换器, 需要添加到app中
--- iHome/web_html.py
+++ iHome/web_html.py
@@ -10,17 +10,23 @@ html_blue = Blueprint('html_blue', __name__)
 
 # 使用蓝图
 @html_blue.route('/<re(".*"):file_name>')
+# @html_blue.route('/<file_name>')
 def get_static_html(file_name):
     """获取静态文件"""
     # 需求: http://127.0.0.1:5000/login.html
     # 拼接file_name所有路径: '/static/html/login.html'
     # flask默认加载文件路径: '/static/html/login.html'
     # static_file = 'static'
-
+    # 默认路由/, 首页
     if not file_name:
         file_name = 'index.html'
 
-    file_path = 'html/' + file_name
+    # http://127.0.0.1:5000/favicon.ico
+    # 这样ico不能进来就直接return拼接了->http://127.0.0.1:5000/favicon.ico
+    if file_name != 'favicon.ico':
+        file_name = 'html/' + file_name
 
-    return current_app.send_static_file(file_path)
+    # return current_app.send_static_file(file_path)
+    # file_path= u'/html/api/1.0/index.html'
+    return current_app.send_static_file(file_name)
 
