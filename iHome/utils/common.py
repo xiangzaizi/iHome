0e4594c2efe53453e37a3296579398e708a17367
xiangzaizi <skyxiang.jiang@hotmail.com>
2018-04-10 09:58:12 +0800

    用户登录状态:login_required校验用户是否登录

--- iHome/utils/common.py
+++ iHome/utils/common.py
@@ -1,5 +1,9 @@
 # -*- coding:utf-8 -*-
 from werkzeug.routing import BaseConverter
+from flask import session, jsonify, g
+from iHome.utils.response_code import RET
+from functools import wraps
+
 
 
 class RegexConverter(BaseConverter):
@@ -9,3 +13,26 @@ class RegexConverter(BaseConverter):
         # 定义的规则
         self.regex = args[0]
         # 注意自定义的路由转换器, 需要添加到app中
+
+
+
+def login_required(view_func):
+    """校验用户是否是登录用户"""
+
+    # 装饰器装饰一个函数时，会修改该函数的__name__属性
+    # 如果希望装饰器装饰之后的函数，依然保留原始的名字和说明文档,就需要使用wraps装饰器，装饰内存函数
+
+    @wraps(view_func)
+    def wraaper(*args, **kwargs):
+        # 获取user_id
+        user_id = session.get('user_id')
+
+        if not user_id:
+            return jsonify(errno=RET.SESSIONERR, errmsg='用户未登录')
+        else:
+            # 表示用户已登录，使用g变量保存住user_id,方便在view_func调用的时候，内部可以直接使用g变量里面的user_id
+            g.user_id = user_id
+
+            return view_func(*args, **kwargs)
+
+    return wraaper
